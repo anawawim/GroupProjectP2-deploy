77a3c5c48977cb74fd61f0d425a9d008f039c507
m-reza1 <mrejaa@gmail.com>
2025-03-05 15:35:50 +0700

    Join, update Room Handler

--- server/roomHandler.js
+++ server/roomHandler.js
@@ -56,9 +56,42 @@ const roomHandler = (io, socket, rooms) => {
       callback(null, room.roomId);
     }
   };
+  const join = (payload, callback) => {
+    const index = rooms.findIndex((room) => room.roomId === payload.roomId);
+    if (index >= 0) {
+      const room = rooms[index];
+      if (room.players[socket.id]) return callback(null);
+
+      if (room.vacant && room.private) {
+        room.players[socket.id] = {
+          option: null,
+          optionLock: false,
+          score: 0,
+        };
+        room.vacant = false;
+        rooms.push(room);
+        socket.join(room.roomId);
+        io.to(room.roomId).emit("room:get", room);
+        callback(null, room);
+      } else {
+        callback({ error: true });
+      }
+    } else {
+      callback({ error: true });
+    }
+  };
+
+  const update = (payload) => {
+    const index = rooms.findIndex((room) => room.roomId === payload.roomId);
+    if (index >= 0) {
+      rooms[index] = payload;
+      io.to(payload.roomId).emit("room:get", payload);
+    }
+  };
 
   socket.on("room:create", create);
-
+  socket.on("room:join", join);
+  socket.on("room:update", update);
 };
 
 module.exports = roomHandler;
